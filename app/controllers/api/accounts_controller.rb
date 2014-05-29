d7d5be036757e328e6366b78db6fb8d5e7027a25
Daniel-Xu <xwd.forever@gmail.com>
2014-05-29 02:05:56 -0400

    basic CRUD

---
+++ app/controllers/api/accounts_controller.rb
@@ -0,0 +1,57 @@
+module API
+  class AccountsController < ApplicationController
+    def index 
+      accounts = Account.all
+      respond_to do |f|
+        f.json {render json: accounts, status: :ok }
+      end
+    end
+
+    def show 
+      account = Account.find(params[:id])
+      respond_to do |f|
+        f.json {render json: account, status: :ok } 
+      end
+    end
+
+    def create 
+      account = Account.new(account_params)
+      if account.save
+        respond_to do |f|
+          f.json {render json: account, status: 201, locatoin: account}
+          #f.json {head 204, locatoin: account}  without entity to make the server faster
+        end
+      else
+        respond_to do |f|
+          f.json {render json: account.errors, status: 422}
+        end
+      end
+    end
+
+    def update
+      account = Account.find(params[:id])
+      if account.update(account_params)
+        respond_to do |f|
+          f.json {render json: account, status: 200}
+        end
+      else
+        respond_to do |f|
+          f.json {render json: account.errors, status: 422}
+        end
+      end
+    end
+
+    def delete 
+      account = Account.find(params[:id])
+      account.destroy
+      head 204
+    end
+
+
+    private 
+      def account_params
+        params.require(:account).permit(:name)
+      end
+  end
+
+end
---
+++ app/controllers/api/domains_controller.rb
@@ -0,0 +1,57 @@
+module API
+  class DomainsController < ApplicationController
+    def index 
+      domains = Domain.all
+      respond_to do |f|
+        f.json { render json: domains, status: :ok }
+      end
+    end
+
+    def show 
+      domain = Domain.find(params[:id])
+
+      respond_to do |f|
+        f.json { render json: domain, status: :ok }
+      end
+    end
+
+    def create 
+      domain = Domain.new(domain_params)
+      if domain.save
+        respond_to do |f|
+          f.json {render json: domain, status: 201, locatoin: domain}
+          #f.json {head 204, locatoin: domain}  without entity to make the server faster
+        end
+      else
+        respond_to do |f|
+          f.json {render json: domain.errors, status: 422}
+        end
+      end
+    end
+
+    def update
+      domain = Domain.find(params[:id])
+      if domain.update(domain_params)
+        respond_to do |f|
+          f.json {render json: domain, status: 200}
+        end
+      else
+        respond_to do |f|
+          f.json {render json: domain.errors, status: 422}
+        end
+      end
+    end
+
+    def delete 
+      domain = Domain.find(params[:id])
+      domain.destroy
+      head 204
+    end
+
+    private 
+      def domain_params
+        params.require(:domain).permit(:hostname, :ipaddr)
+      end
+
+  end
+end
